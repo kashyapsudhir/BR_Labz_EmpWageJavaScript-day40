24d465498a9adcb45fe118c4249572992c2c89e2
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-15 21:54:47 +0530

    [check employee wage based on part time full time]

--- BR_Labz_JS_Program/EmpWage.js
+++ BR_Labz_JS_Program/EmpWage.js
@@ -1,6 +1,7 @@
 console.log("Hello Welcome Employee Wage Program")
 
-//UC-1
+/*
+UC-1
 const Absent = 0;
 let employeeCheck = Math.floor(Math.random()*10)%2;
 if(employeeCheck == Absent)
@@ -15,3 +16,28 @@ else
     console.log("Employee is Present")
 
 }
+*/
+//UC-2
+const Part_Time = 1;
+const Full_time =2;
+const Part_Time_Hours = 4;
+const Full_Time_Hours =8;
+const Wage_Per_Hours = 20;
+
+let Employee_Hours = 0;
+employee_Check = 0;
+employee_Check =Math.floor(Math.random()*10) % 3;
+switch (employee_Check)
+{
+    case Part_Time:
+        Employee_Hours = Part_Time_Hours;
+        break;
+    case Full_time:
+        Employee_Hours = Full_Time_Hours;
+        break;
+    default:
+        Employee_Hours = 0;    
+
+}
+let Employee_Wage =Employee_Hours * Wage_Per_Hours;
+console.log("Employee: " + Employee_Wage);
