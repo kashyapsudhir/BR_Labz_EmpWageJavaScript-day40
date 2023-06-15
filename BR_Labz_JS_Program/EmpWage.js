1e9e407f7e83d615cf6e2f55cae0fa8c377aa266
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-15 21:36:51 +0530

    [check employee is absent or present by using random function]

--- BR_Labz_JS_Program/EmpWage.js
+++ BR_Labz_JS_Program/EmpWage.js
@@ -1,1 +1,17 @@
 console.log("Hello Welcome Employee Wage Program")
+
+//UC-1
+const Absent = 0;
+let employeeCheck = Math.floor(Math.random()*10)%2;
+if(employeeCheck == Absent)
+{
+    console.log("Employee is Absent");
+    return;
+
+
+}
+else
+{
+    console.log("Employee is Present")
+
+}
