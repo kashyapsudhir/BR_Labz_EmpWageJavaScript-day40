4e1b37000b0576d675e0a105c518ad3be29ee877
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-16 08:39:02 +0530

    [check employee wage useing while loop]

--- BR_Labz_JS_Program/EmpWage.js
+++ BR_Labz_JS_Program/EmpWage.js
@@ -69,7 +69,7 @@ Employee_Hours = getWorkingHours(employee_Check);
 let Employee_Wage =Employee_Hours * Wage_Per_Hours;
 console.log("Employee: " + Employee_Wage);
 
-*/
+
 //UC-4
 const Part_Time = 1;
 const Full_Time =2;
@@ -101,5 +101,44 @@ for(let day = 0;
     }
     let Employee_Wage =Employee_Hours * Wage_Per_Hours;
      console.log("Employee: " + Employee_Wage);
+*/
+//UC-5
+const Part_Time = 1;
+const Full_Time =2;
+const Part_Time_Hours = 4;
+const Full_Time_Hours =8;
+const Wage_Per_Hours = 20;
+const Num_Of_Working_Days = 10;
 
+let Total_Employee_Hours = 0;
+
+let Total_Working_Days = 0;
+const Max_Hours_In_Month = 100;
+
+function getWorkingHours(employee_Check)
+{
+    switch(employee_Check)
+    {
+        case Part_Time:
+            return Part_Time;
+        case Full_Time:
+            return Full_Time; 
+        default:
+            return 0;     
+    }
+}
+
+
+while ( Total_Employee_Hours<= Max_Hours_In_Month && Total_Working_Days < Num_Of_Working_Days) 
+{ 
+    Total_Employee_Hours++;
+
+     let employee_Check = Math.floor(Math.random() * 10) % 3;
+
+    Total_Employee_Hours += getWorkingHours (employee_Check);
+
+}
+
+let Employee_Wage = Total_Employee_Hours * Wage_Per_Hours; 
+console.log("UC5 Total Days:" + Total_Working_Days + "Total Hrs:" + Total_Employee_Hours + "Emp Wage:" + Employee_Wage);
     
